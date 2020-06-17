a82238602fa85ef601312e5bff6993ba9654c050
1713011230 <1522393128@qq.com>
2020-06-17 18:06:52 +0800

    张杰第一次作业

--- src/day03/Sigma.java
+++ src/day03/Sigma.java
@@ -4,14 +4,16 @@ public class Sigma {
     public static void main(String[] args) {
         int i,j;
         int sum = 2;
-        for(i=2;i<=100;i++){
-            j=2;
-            while(i%j!=0) {
-                j++;
-                if (j == i) {
-                    sum = sum + i;
+        for(i=3;i<=100;i++){
+            for(j=2;j<=i-1;j++){
+                if(i%j==0){
+                    break;
+                }
+                if(j==i-1){
+                   sum=sum + i;
                 }
             }
+
         }
 
         System.out.println("一百以内质数之和为"+sum);
